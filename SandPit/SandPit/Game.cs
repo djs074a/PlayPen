bc668e6ffb45beec3c1a0725a5e80ba2d76aafbe
dspencer@ivisgroup.com <david.spencer@ivisgroup.com>
2015-10-06 15:05:53 +0100

    Refactoring
    tidy up the code in line with coding standards

--- SandPit/SandPit/Game.cs
+++ SandPit/SandPit/Game.cs
@@ -3,60 +3,59 @@ namespace SandPit
 {
     class Game
     {
-        private int score;
         private int[] rolls = new int[21];
-        private int currentRoll = 0;
+        private int _currentRoll;
         public void Roll(int pins)
         {
-            rolls[currentRoll++] = pins;
+            rolls[_currentRoll++] = pins;
         }
 
         public int Score()
         {
-            int score = 0;
-            int rollCount = 0;
-            for (int frame = 0; frame < 10; frame++)
+            var score = 0;
+            var rollCount = 0;
+            for (var frame = 0; frame < 10; frame++)
             {
-                if (isStrike(rollCount))
+                if (IsStrike(rollCount))
                 {
-                    score += scoreStrike(rollCount);
+                    score += ScoreStrike(rollCount);
                     rollCount += 1;
                 }
-                else if (isSpare(rollCount))
+                else if (IsSpare(rollCount))
                 {
-                    score += scoreSpare(rollCount);
+                    score += ScoreSpare(rollCount);
                     rollCount += 2;
                 }
                 else
                 {
-                    score += scoreFrame(rollCount);
+                    score += ScoreFrame(rollCount);
                     rollCount += 2;
                 }
             }
             return score;
         }
 
-        private int scoreFrame(int rollCount)
+        private int ScoreFrame(int rollCount)
         {
             return rolls[rollCount] + rolls[rollCount + 1];
         }
 
-        private int scoreSpare(int rollCount)
+        private int ScoreSpare(int rollCount)
         {
             return 10 + rolls[rollCount + 2];
         }
 
-        private int scoreStrike(int rollCount)
+        private int ScoreStrike(int rollCount)
         {
             return 10 + rolls[rollCount + 1] + rolls[rollCount + 2];
         }
 
-        private bool isStrike(int rollCount)
+        private bool IsStrike(int rollCount)
         {
             return rolls[rollCount] == 10;
         }
 
-        private bool isSpare(int rollCount)
+        private bool IsSpare(int rollCount)
         {
             return rolls[rollCount] + rolls[rollCount + 1] == 10;
         }
